1d17f30aa4450988c2c73c4aef7c37d02ee9afd1
Sudhakar Rao <52300811+SudhakarRao-SDL-MS@users.noreply.github.com>
2021-04-06 15:44:44 -0700

    Add files via upload

---
+++ OpenXMLSample/OpenXMLSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenXMLSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report report = new Report();
+
+            report.CreateExcelDoc(@"D:\Report.xlsx");
+
+            Console.WriteLine("Excel file has created!");
+        }
+    }
+}
